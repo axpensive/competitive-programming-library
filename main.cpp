9ba2c2f2ef85a3050bff1725329123cc172de900
axpensive <35427211+axpensive@users.noreply.github.com>
2020-01-24 02:34:18 +0900

    素因数分解追加

--- main.cpp
+++ main.cpp
@@ -43,6 +43,19 @@ double calculateDegree(double y, double x) {
 	return degree;
 }
 
+// 素因数分解
+vector<pair<long long, long long> > prime_factorize(long long n) {
+    vector<pair<long long, long long> > res;
+    for (long long p = 2; p * p <= n; ++p) {
+        if (n % p != 0) continue;
+        int num = 0;
+        while (n % p == 0) { ++num; n /= p; }
+        res.push_back(make_pair(p, num));
+    }
+    if (n != 1) res.push_back(make_pair(n, 1));
+    return res;
+}
+
 
 int main() {
 }
